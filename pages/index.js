f3e3db39793ab81c5df4696127374022e276eca8
Do <d.altun@hotmailnl>
2023-04-05 13:08:46 +0200

    Changed prompt response to json and did some formatting in page

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export async function getStaticProps() {
 function Home() {
   const [wInfo, setwInfo] = useState();
   const [cName, setcName] = useState();
+  const [cAlignment, setcAlignment] = useState();
   const [cContext, setcContext] = useState();
   const [cBackstory, setcBackstory] = useState();
   const [cExpertise, setcExpertise] = useState();
@@ -25,6 +26,7 @@ function Home() {
 
   const [wInfoActive, setwInfoActive] = useState(false);
   const [cNameActive, setcNameActive] = useState(false);
+  const [cAlignmentActive, setcAlignmentActive] = useState(false);
   const [cContextActive, setcContextActive] = useState(false);
   const [cBackstoryActive, setcBackstoryActive] = useState(false);
   const [cExpertiseActive, setcExpertiseActive] = useState(false);
@@ -32,8 +34,9 @@ function Home() {
   const [cPersonalityActive, setcPersonalityActive] = useState(false);
   const [cLooksActive, setcLooksActive] = useState(false);
 
-  const [result, setResult] = useState({ result: "" });
+  const [result, setResult] = useState({ result: { cName: "", cAlignment:"", cClass: "", cLooks: "", cPersonality: "", cBackstory: "", cContext: "", cExpertise: "" } });
   const [submitting, setSubmitting] = useState(false);
+  const [genDone, setGenDone] = useState(false);
 
   const [submitting2, setSubmitting2] = useState(false);
 
@@ -59,6 +62,7 @@ function Home() {
       const result = await httpsCallableFromURL(functions, 'https://getnpc-bdwuntrh4a-uc.a.run.app')({
         wInfo: wInfo,
         cName: cName,
+        cAlignment: cAlignment,
         cContext: cContext,
         cBackstory: cBackstory,
         cExpertise: cExpertise,
@@ -71,6 +75,7 @@ function Home() {
         alert(data.error);
       else
         setResult(data);
+        setGenDone(true);
     } catch (error) {
       // Getting the Error details.
       const code = error.code;
@@ -81,6 +86,50 @@ function Home() {
     };
     setSubmitting(false);
   }
+
+  const Result = () => {
+    let json = JSON.stringify(result.result);
+    let obj = JSON.parse(JSON.parse(json));
+
+    console.log(obj);
+    console.log(typeof obj);
+
+    return (      
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{obj.cName}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{obj.cClass}</h6>
+            <p class="card-text">{obj.cLooks}</p>
+            <p class="card-text">{obj.cPersonality}</p>
+            <p class="card-text">{obj.cBackstory}</p>
+            <p class="card-text">{obj.cContext}</p>
+            <p class="card-text">{obj.cExpertise}</p>
+          </div>
+        </div>
+    );
+  }
+
+  const Field = (name, setActive, setValue) => {
+    return (
+      <div class="col text-end">
+        <div class="btn-group" role="group">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name={"btn" + name} id={"button-use-" + name} onClick={function (event) { setValue(""); setActive(true) }}></input>
+            <label class="form-check-label" for={"btn-use-" + name}>use</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name={"btn" + name} id={"btn-gen-" + name} onClick={function (event) { setValue("generate"); setActive(false) }}></input>
+            <label class="form-check-label" for={"btn-gen-" + name}>gen</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name={"btn" + name} id={"btn-no-" + name} onClick={function (event) { setValue(undefined); setActive(false) }} checked></input>
+            <label class="form-check-label" for={"btn-no-" + name}>no</label>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
 
   return (
     <div>
@@ -97,36 +146,38 @@ function Home() {
         <h3>Create my NPC</h3>
         <div class="container">
           <div class="row">
-            <div class="name-tf mb-3">
-              <div class="col">
-                <label for="characterNameTF" class="form-label">Name</label>
-              </div>
-              <div class="col">
-                <div class="btn-group" role="group">
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="btn-cname" id="btn-use-cname" onClick={function (event) { setcNameActive(true) }}></input>
-                      <label class="form-check-label" for="btn-use-cname">use</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="btn-cname" id="btn-gen-cname" onClick={function (event) { setcName("generate"); setcNameActive(false) }}></input>
-                      <label class="form-check-label" for="btn-gen-cname">gen</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="btn-cname" id="btn-no-cname" onClick={function (event) { setcName(""); setcName(undefined); setcNameActive(false) }} checked></input>
-                      <label class="form-check-label" for="btn-no-cname">no</label>
-                  </div>
+            <div class="name-tf mb-3 col">
+              <div class="row">
+                <div class="col">
+                  <label for="characterNameTF" class="form-label">Name</label>
                 </div>
+                {Field("cName", setcNameActive, setcName)}
               </div>
               <input type="textarea" class="form-control" name="characterName" id="characterNameTF"
                 value={cName} onChange={(e) => setcName(e.target.value)} disabled={!cNameActive} placeholder="ex: Aurora, Vandrian, Ko'li'varanja" />
+            </div>
+            <div class="alignment-tf mb-3 col">
+              <div class="row">
+                <div class="col">
+                  <label for="characterAlignmentTF" class="form-label">Alignment</label>
+                </div>
+                {Field("cAlignment", setcAlignmentActive, setcAlignment)}
+              </div>
+              <input type="textarea" class="form-control" name="characterAlignment" id="characterAlignmentTF"
+                value={cAlignment} onChange={(e) => setcAlignment(e.target.value)} disabled={!cAlignmentActive} placeholder="ex: Evil but sweet, Good but naughty, True Neutral, Lawful Evil" />
             </div>
           </div>
           <div class="row">
             <div class="col">
               <div class="world-info-tf mb-3">
-                <label for="WorldContextTF" class="form-label">World context</label>
+                <div class="row">
+                  <div class="col">
+                    <label for="WorldContextTF" class="form-label">World context</label>
+                  </div>
+                  {Field("wInfo", setwInfoActive, setwInfo)}
+                </div>
                 <textarea type="textarea" class="form-control" name="worldInfo" id="WorldContextTF" aria-describedby="WorldContextTFHelp"
-                  value={wInfo} onChange={(e) => setwInfo(e.target.value)} placeholder="ex: A medieval fantasy world called WorldName where magic exists and electrical technology does not exist"
+                  value={wInfo} onChange={(e) => setwInfo(e.target.value)} disabled={!wInfoActive} placeholder="ex: A medieval fantasy world called WorldName where magic exists and electrical technology does not exist"
                   rows="8" style={{ fontSize: 16 + "px" }}></textarea>
                 <div id="WorldContextTFHelp" class="form-text">Try to explain it in a proper story format like the example"</div>
               </div>
@@ -135,19 +186,40 @@ function Home() {
               <div class="quirks-container">
                 <div calss="container">
                   <div class="row expertise-tf mb-3">
-                    <label for="characterExpertiseTF" class="form-label">Expertise</label>
-                    <input type="textarea" class="form-control" name="characterExpertise" id="characterExpertiseTF"
-                      value={cExpertise} onChange={(e) => setcExpertise(e.target.value)} placeholder="ex: Lockpicking, politics, warfare, gambling" />
+                    <div class="row">
+                      <div class="col">
+                        <label for="characterExpertiseTF" class="form-label">Expertise</label>
+                      </div>
+                      {Field("cExpertise", setcExpertiseActive, setcExpertise)}
+                    </div>
+                    <div class="col">
+                      <input type="textarea" class="form-control" name="characterExpertise" id="characterExpertiseTF"
+                        value={cExpertise} onChange={(e) => setcExpertise(e.target.value)} disabled={!cExpertiseActive} placeholder="ex: Lockpicking, politics, warfare, gambling" />
+                    </div>
                   </div>
                   <div class="row class-tf mb-3">
-                    <label for="characterClassTF" class="form-label">Class and/or profession</label>
-                    <input type="textarea" class="form-control" name="characterClass" id="characterClassTF"
-                      value={cClass} onChange={(e) => setcClass(e.target.value)} placeholder="ex: cook, wizard" />
+                    <div class="row">
+                      <div class="col">
+                        <label for="characterClassTF" class="form-label">Class and/or profession</label>
+                      </div>
+                      {Field("cClass", setcClassActive, setcClass)}
+                    </div>
+                    <div class="col">
+                      <input type="textarea" class="form-control" name="characterClass" id="characterClassTF"
+                        value={cClass} onChange={(e) => setcClass(e.target.value)} disabled={!cClassActive} placeholder="ex: cook, wizard" />
+                    </div>
                   </div>
                   <div class="row personality-tf mb-3">
-                    <label for="characterPersonalityTF" class="form-label">Personality traits</label>
-                    <input type="textarea" class="form-control" name="characterPersonality" id="characterPersonalityTF" aria-describedby="characterPersonalityTFHelp"
-                      value={cPersonality} onChange={(e) => setcPersonality(e.target.value)} placeholder="ex: Flamboyant, Tsundere, Bashful" />
+                    <div class="row">
+                      <div class="col">
+                        <label for="characterPersonalityTF" class="form-label">Personality traits</label>
+                      </div>
+                      {Field("cPersonality", setcPersonalityActive, setcPersonality)}
+                    </div>
+                    <div class="col">
+                      <input type="textarea" class="form-control" name="characterPersonality" id="characterPersonalityTF" aria-describedby="characterPersonalityTFHelp"
+                        value={cPersonality} onChange={(e) => setcPersonality(e.target.value)} disabled={!cPersonalityActive} placeholder="ex: Flamboyant, Tsundere, Bashful" />
+                    </div>
                     <div id="characterPersonalityTFHelp" class="form-text">Try to stick to singular words or small sentences like "always skipping about"</div>
                   </div>
                 </div>
@@ -157,18 +229,28 @@ function Home() {
           <div class="row">
             <div class="col">
               <div class="backstory-tf mb-3">
-                <label for="characterBackstoryTF" class="form-label">Backstory elements</label>
+                <div class="row">
+                  <div class="col">
+                    <label for="characterBackstoryTF" class="form-label">Backstory elements</label>
+                  </div>
+                  {Field("cBackstory", setcBackstoryActive, setcBackstory)}
+                </div>
                 <textarea type="textarea" class="form-control" name="characterBackstory" id="characterBackstoryTF" aria-describedby="characterBackstoryTFHelp"
-                  value={cBackstory} onChange={(e) => setcBackstory(e.target.value)} placeholder="ex: Has slain a raging bull in his youth and met a young dragon up in the mountains called Verial"
+                  value={cBackstory} onChange={(e) => setcBackstory(e.target.value)} disabled={!cBackstoryActive} placeholder="ex: Has slain a raging bull in his youth and met a young dragon up in the mountains called Verial"
                   rows="5" style={{ fontSize: 16 + "px" }}></textarea>
                 <div id="characterBackstoryTFHelp" class="form-text">Try to stick to a nice short summarisation"</div>
               </div>
             </div>
             <div class="col">
               <div class="context-tf mb-3">
-                <label for="characterContextTF" class="form-label">Context</label>
+                <div class="row">
+                  <div class="col">
+                    <label for="characterContextTF" class="form-label">Context</label>
+                  </div>
+                  {Field("cContext", setcContextActive, setcContext)}
+                </div>
                 <textarea type="textarea" class="form-control" name="characterContext" id="characterContextTF" aria-describedby="characterContextTFHelp"
-                  value={cContext} onChange={(e) => setcContext(e.target.value)} placeholder="ex: lives in the royal capital with his mother and 2 younger sisters. Often hangs out with their friend Gary."
+                  value={cContext} onChange={(e) => setcContext(e.target.value)} disabled={!cContextActive} placeholder="ex: lives in the royal capital with his mother and 2 younger sisters. Often hangs out with their friend Gary."
                   rows="5" style={{ fontSize: 16 + "px" }}></textarea>
                 <div id="characterContextTFHelp" class="form-text">Anything external that is important to this character's story</div>
               </div>
@@ -176,9 +258,14 @@ function Home() {
           </div>
           <div class="row">
             <div class="looks-tf mb-3">
-              <label for="characterLooksTF" class="form-label">Looks</label>
+              <div class="row">
+                <div class="col">
+                  <label for="characterLooksTF" class="form-label">Looks</label>
+                </div>
+                {Field("cLooks", setcLooksActive, setcLooks)}
+              </div>
               <input type="textarea" class="form-control" name="characterLooks" id="characterLooksTF" aria-describedby="characterLooksTFHelp"
-                value={cLooks} onChange={(e) => setcLooks(e.target.value)} placeholder="ex: a woman with long red hair wearing an office suit, sunglasses and a ton of accessories" />
+                value={cLooks} onChange={(e) => setcLooks(e.target.value)} disabled={!cLooksActive} placeholder="ex: a woman with long red hair wearing an office suit, sunglasses and a ton of accessories" />
               <div id="characterLooksTFHelp" class="form-text">Try to describe it within a single sentence</div>
             </div>
           </div>
@@ -187,8 +274,8 @@ function Home() {
         <div class="d-grid gap-2 col-6 mx-auto">
           <button disabled={submitting} onClick={getNPC} class="btn btn-primary" type="button">Generate npc</button>
         </div>
-        <div className={styles.result}>
-          {result.result}
+        <div>
+          { genDone && <Result /> }
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous" />
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const openAI = require("openai");
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const { onCall, HttpsError } = require("firebase-functions/v2/https");
+const { json } = require("express");
 
 exports.getnpc = onCall((request) => {
     const Configuration = openAI.Configuration;
@@ -45,80 +46,94 @@ exports.getnpc = onCall((request) => {
     });
 });
 
-function isEmpty(input) {
-    return (input == "")
+function needsGen(input) {
+    return (input == "generate")
 }
 
 function isUsed(input) {
-    return (input == "generate")
+    return (input != "" && input != undefined && input != "generate")
 }
 
 function generatePromptNPC(info) {
     var wInfoString = "The world the character lives in "
-    if (isEmpty(info.wInfo))
-        wInfoString = "is unknown to me."
-    else if (isUsed(input))
-        wInfoString = ": " + info.wInfo;
+    if (needsGen(info.wInfo))
+        wInfoString += "is unknown to me."
+    else if (isUsed(info.wInfo))
+        wInfoString += ": " + info.wInfo;
     else
         wInfoString = ""
 
     var nameString = ""
-    if (isEmpty(info.cName))
+    if (needsGen(info.cName))
         nameString = "Please think of a name for my character."
-    else if (isUsed(input))
+    else if (isUsed(info.cName))
         nameString = "The character's name is : " + info.cName
     else
         nameString = "Please don't give the character a name, just refer to them as they/them."
 
     var contextString = ""
-    if (isEmpty(info.cContext))
+    if (needsGen(info.cContext))
         contextString = "Please think of some context for the character, like parents, friends and pets."
-    else if (isUsed(input))
+    else if (isUsed(info.cContext))
         contextString = "A bit of context about their life : " + info.cContext
 
     var backstoryString = ""
-    if (isEmpty(info.cBackstory))
+    if (needsGen(info.cBackstory))
         backstoryString = "Please think of a proper backstory for this character, like some big events they went through."
-    else if (isUsed(input))
+    else if (isUsed(info.cBackstory))
         backstoryString = "The character's backstory : " + info.cBackstory
 
     var expertiseString = ""
-    if (isEmpty(info.cExpertise))
+    if (needsGen(info.cExpertise))
         expertiseString = "Please think of some expertise for this character."
-    else if (isUsed(input))
+    else if (isUsed(info.cExpertise))
         expertiseString = "They are experts at : " + info.cExpertise
 
     var classString = ""
-    if (isEmpty(info.cClass))
+    if (needsGen(info.cClass))
         classString = "Please think of a class and/or profession for this character."
-    else if (isUsed(input))
+    else if (isUsed(info.cClass))
         classString = "The character's class and/or profession is " + info.cClass
 
     var personalityString = ""
-    if (isEmpty(info.cPersonality))
+    if (needsGen(info.cPersonality))
         personalityString = "Please think of some personality traits for this character."
-    else if (isUsed(input))
+    else if (isUsed(info.cPersonality))
         personalityString = "Some personality traits for this character: " + info.cPersonality
 
     var looksString = ""
-    if (isEmpty(info.cLooks))
+    if (needsGen(info.cLooks))
         looksString = "Please think of what this character would physically look like."
-    else if (isUsed(input))
+    else if (isUsed(info.cLooks))
         looksString = "The character's looks : " + info.cLooks
 
+    var alignment = ""
+    if (needsGen(info.cAlignment))
+        alignment = "Please keep in mind that the character's alignment is unknown to me but they do need one."
+    else if (isUsed(info.cAlignment))
+        alignment = "The character's alignment is :" + info.cAlignment + ". Please keep that in mind with all your other answers."
+
+    let json = {
+        "wInfo": wInfoString,
+        "cName": nameString,
+        "cAlignment": alignment,
+        "cContext": contextString,
+        "cBackstory":  backstoryString,
+        "cExpertise": expertiseString,
+        "cClass": classString,
+        "cPersonality": personalityString,
+        "cLooks": looksString
+    }
 
     const prompt = `
-    I need to create a character for my tabletop RPG game. Here is some info about the character:
-    ${wInfoString}
-    ${nameString}
-    ${contextString}
-    ${backstoryString}
-    ${expertiseString}
-    ${classString}
-    ${personalityString}
-    ${looksString}
+    I need to create a character for my tabletop RPG game. Here is some info about the character put into a json object.
 
-    Please answer all these questions for me and deliver it as a nicely laid out story of around 500 words split into the given categories.
+    ${JSON.stringify(json, null, 2)}
+
+    "wInfo" speaks about the world the character lives in, "cName" is the name of the character, "cContext" is some context about the character's life, "cBackstory" is the character's backstory, "cExpertise" is the character's expertise, "cClass" is the character's class and/or profession, "cPersonality" is the character's personality traits and "cLooks" is what the character looks like.
+    Replace the current values of the json object with the answers or info you have given me in the corresponding key and give that back as your only answer so I can parse it in my code please. no need for styling outside of the json values.
+    Please make your answers as much of a story as possible, like a small paragraph, and don't forget to include the name of the character in the story. The backstory can be a bit longer.
+    I do not need any info about the world, so wInfo can be left empty. I would also like the backstory to be a bit longer than the rest of the info and contain at least 3 life events. I would like to have at least some friends and family, including names, in the context part.
     `
     console.log(prompt);
     return prompt;
